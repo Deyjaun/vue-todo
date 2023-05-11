25ead8c5aba95836a5cfdbe13b5ee0d337faeee0
Deyjaun Lawrence <Dlawrence@brooklynsteamcenter.org>
2023-05-11 14:56:37 -0400

    firzt

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import {ref} from 'vue'
+
+let todos = ref(['','',''])
+let touch = ref('')
+
+function uno(){
+  todos.value.push(touch.value)
+  touch.value= ''
+}
+
+function deleteI (index) {
+  todos.value.splice(index, 1)
+
+}
+</script>
+
+<template>
+<h1>My Todo Application</h1>
+<ol>
+<li v-for="(todo, index) in todos">
+  <button @click="deleteI">delete</button>
+  {{todo}}
+  {{ index }}
+</li>
+</ol>
+<input v-model="touch" @keydown.enter="uno">
+<button @click="uno">Add Todo</button>
+</template>
+
+<style>
+body {
+    font-size: large;
+color: #000000;
+background-color: #0D00FF;
+background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg stroke='%23FFFFFF' stroke-width='100' stroke-opacity='0' %3E%3Ccircle fill='%230D00FF' cx='0' cy='0' r='1800'/%3E%3Ccircle fill='%230041ff' cx='0' cy='0' r='1700'/%3E%3Ccircle fill='%23005dff' cx='0' cy='0' r='1600'/%3E%3Ccircle fill='%230073ff' cx='0' cy='0' r='1500'/%3E%3Ccircle fill='%230084ff' cx='0' cy='0' r='1400'/%3E%3Ccircle fill='%230093ff' cx='0' cy='0' r='1300'/%3E%3Ccircle fill='%2300a0ff' cx='0' cy='0' r='1200'/%3E%3Ccircle fill='%2300abff' cx='0' cy='0' r='1100'/%3E%3Ccircle fill='%2300b6ff' cx='0' cy='0' r='1000'/%3E%3Ccircle fill='%2300c0ff' cx='0' cy='0' r='900'/%3E%3Ccircle fill='%2300c9fb' cx='0' cy='0' r='800'/%3E%3Ccircle fill='%2300d3df' cx='0' cy='0' r='700'/%3E%3Ccircle fill='%2300dcc0' cx='0' cy='0' r='600'/%3E%3Ccircle fill='%2300e4a2' cx='0' cy='0' r='500'/%3E%3Ccircle fill='%2300ec82' cx='0' cy='0' r='400'/%3E%3Ccircle fill='%2300f462' cx='0' cy='0' r='300'/%3E%3Ccircle fill='%2300fa40' cx='0' cy='0' r='200'/%3E%3Ccircle fill='%233DFF00' cx='0' cy='0' r='100'/%3E%3C/g%3E%3C/svg%3E");
+background-attachment: fixed;
+background-size: cover;
+}
+button {
+    color: rgb(255, 255, 255);
+    background-color: rgb(255, 0, 0);
+    font-size: 100%;
+    font-weight: bold;
+    border-radius: 25%;
+    border-color: rgb(255, 0, 0);
+    border-width: 7px;
+    }
+</style>
+
+
